ceb0aeb8c8500e33f4425e7f00906f138fb25a9e
Шелков Дмитрий <bobyorrr@gmail.com>
2020-12-07 10:35:51 +0300

    .menu-primary правки

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,33 @@
-let text = "Привет Васян";
-console.log(text);
+// Класс для активации
+const activeClass = "active";
+
+// menu-primary аккардион меню
+const menuDropControl = document.querySelectorAll("[data-drop-control]");
+
+if (menuDropControl) {
+	for (const control of menuDropControl) {
+		const container = document.querySelector(`[data-drop-container="${control.dataset.dropControl}"]`);
+
+		if (container.dataset.dropActive === "true") {
+			container.style.height = container.scrollHeight + "px";
+			container.classList.add(activeClass);
+			control.classList.add(activeClass);
+		} else if (container.dataset.dropActive === "false") {
+			container.style.height = "0px";
+			container.classList.remove(activeClass);
+			control.classList.remove(activeClass);
+		}
+
+		control.addEventListener("click", () => {
+			if (container.dataset.dropActive === "true") {
+				container.style.height = "0px";
+				container.dataset.dropActive = "false";
+			} else if (container.dataset.dropActive === "false") {
+				container.style.height = container.scrollHeight + "px";
+				container.dataset.dropActive = "true";
+			}
+			container.classList.toggle(activeClass);
+			control.classList.toggle(activeClass);
+		});
+	}
+}
